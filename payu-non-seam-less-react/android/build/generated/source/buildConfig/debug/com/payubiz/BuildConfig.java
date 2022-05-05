c3a56041c124ce018b60a33945a47154e21f917e
rohit <rcrchahal@gmail.com>
2022-05-05 18:59:43 +0530

    initial commit

---
+++ payu-non-seam-less-react/android/build/generated/source/buildConfig/debug/com/payubiz/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.payubiz;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.payubiz";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String VERSION_NAME = "2.4.0";
+}
